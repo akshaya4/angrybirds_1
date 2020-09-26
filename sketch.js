66dc121af36dd6f43d97b91cb8fe051ecb6f2b5f
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-26 17:00:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var box1, pig1;
 var backgroundImg;
 
 
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
@@ -35,7 +38,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
     
     box1.display();

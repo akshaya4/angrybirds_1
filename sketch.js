a3f82a9c1d0a055881467491fa441643e51d1af6
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-26 16:21:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,10 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+var backgroundImg;
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
